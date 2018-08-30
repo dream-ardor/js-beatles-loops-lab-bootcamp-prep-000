1dc3bc300a504fffbe597f19b6a7ed1412c3b7fa
Dan Barrick <barrick.dan80@gmail.com>
2018-08-30 22:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-'use strict';
-
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-    array.push(musicians[i] + " plays " + instruments[i]);
-  }
-  return array;
+function iLoveTheBeatles(n){
+  var newArr = [];
+  
+  do{
+    newArr.push("I love the Beatles!");
+    n++;
+    console.log(n);
+    // for(var i = n; i >= 0; i--){
+    //     n--;
+    //     newArr.push("I love the Beatles!");
+    //   }
+  }while(n<15);
+  
+  return newArr;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
-  }
-  return array;
-}
-
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    number += 1
-  } while (number < 15);
-  return array;
-}
+iLoveTheBeatles(55);
